83c678534c29de3d37d69f9bddd0fd210603a074
romeshjainn <romeshjainofficial@gmail.com>
2023-08-08 20:30:17 +0530

    new version

--- arrNum.js
+++ arrNum.js
@@ -1,42 +1,26 @@
 let arr = [1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 1, 1, 0, 0];
-let obj = {};
 
-for (let i = 0; i < arr.length; i++) {
-  obj[arr[i]] = (obj[arr[i]] || 0) + 1;
-}
-
-console.log(obj);
-
-arr = [];
-console.log(arr);
-
-for (let key in obj) {
-  for (let i = 0; i < obj[key]; i++) {
-    arr.push(key);
+function arrangeArr(arr) {
+  let obj = {};
+  for (let i = 0; i < arr.length; i++) {
+    obj[arr[i]] = (obj[arr[i]] || 0) + 1;
   }
+
+  arr = [];
+  for (let key in obj) {
+    for (let i = 0; i < obj[key]; i++) {
+      arr.push(parseInt(key));
+    }
+  }
+
+  return arr;
 }
 
-console.log(arr);
+console.log(arrangeArr(arr));
+
 // output
-// [
-//   "0",
-//   "0",
-//   "0",
-//   "0",
-//   "0",
-//   "0",
-//   "0",
-//   "0",
-//   "1",
-//   "1",
-//   "1",
-//   "1",
-//   "1",
-//   "1",
-//   "1",
-//   "1",
-//   "1",
-// ];
+// [0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1];
+
 // function arrangeArr(arr) {
 //   let newArr = [];
 
