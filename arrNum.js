c9605ed86f66f94edd883384f1762ed3f5098a2a
romeshjainn <romeshjainofficial@gmail.com>
2023-08-08 20:24:16 +0530

    new version

--- arrNum.js
+++ arrNum.js
@@ -1,25 +1,62 @@
 let arr = [1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 1, 1, 0, 0];
+let obj = {};
 
-function arrangeArr(arr) {
-  let newArr = [];
-
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[0] === arr[i]) {
-      newArr.push(arr[i]);
-    }
-  }
-
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[0] !== arr[i]) {
-      newArr.push(arr[i]);
-    }
-  }
-
-  return newArr;
+for (let i = 0; i < arr.length; i++) {
+  obj[arr[i]] = (obj[arr[i]] || 0) + 1;
 }
 
-let result = arrangeArr(arr);
-console.log(result);
+console.log(obj);
 
-// Output 
-// [1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0];
+arr = [];
+console.log(arr);
+
+for (let key in obj) {
+  for (let i = 0; i < obj[key]; i++) {
+    arr.push(key);
+  }
+}
+
+console.log(arr);
+// output
+// [
+//   "0",
+//   "0",
+//   "0",
+//   "0",
+//   "0",
+//   "0",
+//   "0",
+//   "0",
+//   "1",
+//   "1",
+//   "1",
+//   "1",
+//   "1",
+//   "1",
+//   "1",
+//   "1",
+//   "1",
+// ];
+// function arrangeArr(arr) {
+//   let newArr = [];
+
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[0] === arr[i]) {
+//       newArr.push(arr[i]);
+//     }
+//   }
+
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[0] !== arr[i]) {
+//       newArr.push(arr[i]);
+//     }
+//   }
+
+//   return newArr;
+// }
+
+// let result = arrangeArr(arr);
+// console.log(result);
+
+// // Output
+// // [1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0];
