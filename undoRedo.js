58ed51d3ffdd5dcef1ed212af6a260c3e58fed29
romeshjainn <romeshjainofficial@gmail.com>
2023-08-10 10:12:13 +0530

    update

---
+++ undoRedo.js
@@ -0,0 +1,16 @@
+// function popAndStore(arr) {
+//   if (arr.length === 0) {
+//     return null; // Return null if the array is empty
+//   }
+
+//   const poppedValue = arr.pop(); // Pop the last element from the array and store it
+
+//   return poppedValue; // Return the popped value
+// }
+
+// // Example usage
+// const myArray = [1, 2, 3, 4, 5];
+// const poppedItem = popAndStore(myArray);
+// console.log(`Popped value: ${poppedItem}`); // This will log the popped value to the console
+// console.log(`Updated array: ${myArray}`); // This will log the array after popping
+let
