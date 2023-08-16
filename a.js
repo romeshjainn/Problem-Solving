0fd486bfcd05cf511e9bda5848dde94d71a0debd
romeshjainn <romeshjainofficial@gmail.com>
2023-08-16 19:10:07 +0530

    update

--- a.js
+++ a.js
@@ -1,7 +1,7 @@
 let arr = [5, 2, 3, 1, 6, 7];
 let num = 4;
-let a = (arr.length - 1) / 2;
-a = Math.ceil(a);
+// let a = (arr.length - 1) / 2;
+// a = Math.ceil(a);
 let store = 0;
 
 function check(arr, num) {
@@ -10,13 +10,16 @@ function check(arr, num) {
 
     for (let j = 1; j < arr.length; j++) {
       if (store < arr[j]) {
-        store = arr[j];
+          store = arr[j];
+            // console.log(store)
       }
     }
 
     let elemIndex = arr.indexOf(store);
-    arr.splice(elemIndex, 1);
-  }
+      arr.splice(elemIndex, 1);
+    //   console.log(arr)
+    }
+    
   let largestNum = arr[0];
   for (let j = 1; j < arr.length; j++) {
     if (largestNum < arr[j]) {
@@ -26,7 +29,7 @@ function check(arr, num) {
   return largestNum;
 }
 
-let result = check(arr, num);
-console.log(result);
 
-//  https://codeshare.io/MNOMny
+// console.log(arr);
+console.log(check(arr, num));
+
