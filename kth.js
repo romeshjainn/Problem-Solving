704a44950525b011fd771ff391800846b62cc906
romeshjainn <romeshjainofficial@gmail.com>
2023-08-19 08:03:46 +0530

    kth

--- kth.js
+++ kth.js
@@ -1,12 +1,13 @@
-let nums = [1, 3, 7, 4, 5, -1, 7, -8, -3];
+// let nums = [1, 3, 7, 4, 5, -1, 7, -8, -3];
+let nums = [1,2,3,4,4, -1, -8, -3];
 let k = 3;
 let positive = [];
 let negative = [];
 
 const kth = (nums, k) => {
-    for (let i = 0; i < nums.length; i++) {
+    for (let i = 0; i <= nums.length; i++) {
         if (nums[i] >= 0) {
-            positive.push(0);
+            positive.push(null);
             if (!positive[nums[i]]) {
                 positive[nums[i]] = 1;
             } else {
