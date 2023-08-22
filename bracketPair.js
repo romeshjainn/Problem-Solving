3ef75a298ba922debfced8299cdf7897ad87671e
Romesh Jain <romeshjainofficial@gmail.com>
2023-08-22 20:07:47 +0530

    update

--- bracketPair.js
+++ bracketPair.js
@@ -1,4 +1,12 @@
-
+// stored all the opening and closing brackets in seprate arrays
+// storing the opening brackets, in a new array
+// check if the current element is available in closing bracket array
+// popping the store array, storing the index of that array to a new variable
+// storing the index of closing bracket in the closing array
+// return false if the indexes dont match
+// coz the opening and closing bracket's index is same in both arrays
+// returing true, the brackets have their pair 
+ 
 const isValid = (brackets) => {
   let opening = ["{", "(", "["];
   let closing = ["}", ")", "]"];
