011fdf40cc7aa761acf9ff9b779715b3f19c7fa6
Romesh Jain <romeshjainofficial@gmail.com>
2023-08-26 10:54:12 +0530

    update

--- Count n Say Alphabet/v2.js
+++ Count n Say Alphabet/v2.js
@@ -1,14 +1,23 @@
 let store = [];
+let same = [];
 const countAndSay = (arr) => {
   for (let i = 0; i < arr.length; i++) {
-    let count = 1;
     if (arr[i] == arr[i + 1]) {
-      count++;
-    } else {
-      store.push(arr[i]);
+      same.push(arr[i], arr[i + 1]);
+      break;
     }
-    store.push(arr[i], count);
+    for (let j = 0; j < same.length; j++) {
+      let count = 1;
+      if (same[i] == same[i + 1]) {
+        count++;
+      } else {
+        store.push(same[i]);
+      }
+      store.push(same[i], count);
+      same = [];
+    }
   }
+
   console.log(store);
 };
 let arr = ["a", "a", "b", "b", "c", "c", "c"];
