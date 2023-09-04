1b1c689c7b7114a704afd912d36d20ac820fa7bf
Romesh Jain <romeshjainofficial@gmail.com>
2023-09-04 20:45:08 +0530

    update

--- rotate img/rotate.js
+++ rotate img/rotate.js
@@ -3,15 +3,7 @@ let matrix = [
   [4, 5, 6],
   [7, 8, 9],
 ];
-const rotate = (matxix) => {
-    let arr = [];
-    for (let i = matrix.length - 1; i >= 0; i--){
-        let current = matrix[i];
-        for (let j = 0; j < current.length; j++){
-            arr.push(current[j]);
-        }
-    }
-    return arr;
+
+const rotate = (matrix) => {
+    
 };
-// rotate(matrix);
-console.log(rotate(matrix));
